36ac1d3d999236645176d17e8b9319189e69346b
Rinat Abidullin <rinatabidullin@gmail.com>
2020-01-18 03:28:10 +0500

    Эксперимент с передачей пустого паззла решателю судоку: работает как генератор судоку

--- Completed exercises/codewars.com/003 - Sudoku Solver (3 kyu)/Sudoku Solver/Sudoku Solver/main.swift
+++ Completed exercises/codewars.com/003 - Sudoku Solver (3 kyu)/Sudoku Solver/Sudoku Solver/main.swift
@@ -31,7 +31,7 @@ let puzzle2 = [
 [0,2,0,0,5,0,0,7,0]
 ]
 
-let puzzle3 = [
+let hardPuzzle = [
 [0,0,5,3,0,0,0,0,0],
 [8,0,0,0,0,0,0,2,0],
 [0,7,0,0,1,0,5,0,0],
@@ -43,7 +43,19 @@ let puzzle3 = [
 [0,0,0,0,0,9,7,0,0]
 ]
 
+let emptyPuzzle = [
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0]
+]
+
 print("Puzzle: \n")
-printSudoku(puzzle: puzzle3)
+printSudoku(puzzle: emptyPuzzle)
 print("\n\nSolution: \n")
-printSudoku(puzzle: sudokuSolver(puzzle: puzzle3)!)
+printSudoku(puzzle: sudokuSolver(puzzle: emptyPuzzle)!)
