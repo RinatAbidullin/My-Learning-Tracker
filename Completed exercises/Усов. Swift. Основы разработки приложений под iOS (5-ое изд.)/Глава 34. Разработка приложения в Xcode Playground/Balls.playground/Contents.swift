5e4723083b781ac676f60382bfe5b2b9eded7804
Rinat Abidullin <rinatabidullin@gmail.com>
2020-01-25 21:57:49 +0500

    Добавлен проект выполненного интерактивного приложения по главе 34 книги Усова

---
+++ Completed exercises/Усов. Swift. Основы разработки приложений под iOS (5-ое изд.)/Глава 34. Разработка приложения в Xcode Playground/Balls.playground/Contents.swift
@@ -0,0 +1,6 @@
+import PlaygroundSupport
+import UIKit
+
+let balls = Balls(colors: [UIColor.white, UIColor.green, UIColor.yellow, UIColor.red, UIColor.blue])
+
+PlaygroundPage.current.liveView = balls
