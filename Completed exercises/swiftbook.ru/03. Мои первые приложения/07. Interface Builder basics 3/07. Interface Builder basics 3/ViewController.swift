a6978629e83ee2d8cc6bba41376728752e734928
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-02 12:41:22 +0500

    Выполнено задание из урока "07. Interface Builder basics 3"

---
+++ Completed exercises/swiftbook.ru/03. Мои первые приложения/07. Interface Builder basics 3/07. Interface Builder basics 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  07. Interface Builder basics 3
+//
+//  Created by Rinat Abidullin on 02.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
