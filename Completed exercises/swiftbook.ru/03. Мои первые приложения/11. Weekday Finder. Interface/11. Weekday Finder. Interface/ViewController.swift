f6b1629888ef953eff3c839e4cebbb43e1e3ebaa
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-02 13:59:45 +0500

    Выполнено задание из урока "11. Weekday Finder. Interface"

---
+++ Completed exercises/swiftbook.ru/03. Мои первые приложения/11. Weekday Finder. Interface/11. Weekday Finder. Interface/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  11. Weekday Finder. Interface
+//
+//  Created by Rinat Abidullin on 02.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
