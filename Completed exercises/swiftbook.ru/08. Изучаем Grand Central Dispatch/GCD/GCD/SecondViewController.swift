5331dd2932dce4210c3362039d1c31dd6eb196b5
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-18 21:25:44 +0500

    Изучаем Grand Central Dispatch - 06. Интерфейс приложения

---
+++ Completed exercises/swiftbook.ru/08. Изучаем Grand Central Dispatch/GCD/GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Rinat Abidullin on 18.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
