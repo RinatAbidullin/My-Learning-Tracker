b67a3b5d6eefb069baa61d2f8ebeba50405fdbf8
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-18 21:42:58 +0500

    Изучаем Grand Central Dispatch - 07. Проблема приложения

--- Completed exercises/swiftbook.ru/08. Изучаем Grand Central Dispatch/GCD/GCD/SecondViewController.swift
+++ Completed exercises/swiftbook.ru/08. Изучаем Grand Central Dispatch/GCD/GCD/SecondViewController.swift
@@ -10,7 +10,33 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://images4.alphacoders.com/103/1038322.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
     }
 }
