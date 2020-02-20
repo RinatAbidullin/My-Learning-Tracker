a4cb29b0acdf092a199e8bcceae296580196cc65
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-20 22:09:53 +0500

    11. Изучаем систему Git. GitHub

---
+++ Completed exercises/swiftbook.ru/11. Изучаем систему Git. GitHub/GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Rinat Abidullin on 20.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("Another print statement")
+    }
+
+
+}
+
