1d461b14858227075edcb32bf5a170734813f8f2
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-23 14:29:55 +0500

    18. Изучаем паттерн MVVM - 06. MVVM for UITableViewController. Part 1

---
+++ Completed exercises/swiftbook.ru/18. Изучаем паттерн MVVM/MVVM-2/MVVM-2/Protocols/TableViewModelType.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewModelType.swift
+//  MVVM-2
+//
+//  Created by Rinat Abidullin on 23.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewModelType {
+    var numberOfRows: Int { get }
+    var profiles: [Profile] { get }
+}
---
+++ Completed exercises/swiftbook.ru/18. Изучаем паттерн MVVM/MVVM-2/MVVM-2/ViewModel/ViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ViewModel.swift
+//  MVVM-2
+//
+//  Created by Rinat Abidullin on 23.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel: TableViewModelType {
+    
+    var numberOfRows: Int {
+        return profiles.count
+    }
+    
+    
+    var profiles = [
+        Profile(name: "John", secondName: "Smith", age: 33),
+        Profile(name: "Max", secondName: "Kolby", age: 21),
+        Profile(name: "Mark", secondName: "Salmon", age: 55)
+    ]
+}
