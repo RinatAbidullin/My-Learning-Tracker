24d74f80ad12b33112c394837ad2b15bae25cab9
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-29 11:55:57 +0500

    21. TDD. Unit Testing - 22. Cell Configure Tests

--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoApp/View/TaskCell.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoApp/View/TaskCell.swift
@@ -11,8 +11,18 @@ import UIKit
 class TaskCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     func configure(withTask task: Task) {
+        titleLabel.text = task.title
         
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy" // http://nsdateformatter.com
+        if let date = task.date {
+            dateLabel.text = df.string(from: date)
+        }
+        
+        locationLabel.text = task.location?.name
     }
 }
--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/View/TaskCellTests.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/View/TaskCellTests.swift
@@ -36,6 +36,47 @@ class TaskCellTests: XCTestCase {
     func testCellHasTitleLabelInContentView() {
         XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
     }
+    
+    func testCellHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testConfigureSetsTitle() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testConfigureSetsDate() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy" // http://nsdateformatter.com
+        let date = task.date
+        let dateString = df.string(from: date!)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
+    
+    func testConfigureSetsLocation() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.locationLabel.text, task.location?.name)
+    }
 }
 
 extension TaskCellTests {
