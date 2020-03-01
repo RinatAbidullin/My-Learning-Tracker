2e55cbfbec689d5f044f1c5fc95ee013a0a04d30
Rinat Abidullin <rinatabidullin@gmail.com>
2020-03-01 11:42:49 +0500

    25. Testing Labels on DetailViewController

--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoApp/Controller/DetailViewController.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoApp/Controller/DetailViewController.swift
@@ -13,6 +13,17 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    
+    var task: Task!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        titleLabel.text = task.title
+        descriptionLabel.text = task.description
+        locationLabel.text = task.location?.name
+    }
 }
--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/DetailViewControllerTests.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import ToDoApp
 
 class DetailViewControllerTests: XCTestCase {
@@ -24,26 +25,53 @@ class DetailViewControllerTests: XCTestCase {
     }
 
     func testHasTitleLabel() {
-        
         XCTAssertNotNil(sut.titleLabel)
         XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
     }
     
     func testHasDescriptionLabel() {
-        
         XCTAssertNotNil(sut.descriptionLabel)
         XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
     }
     
     func testHasDateLabel() {
-        
         XCTAssertNotNil(sut.dateLabel)
         XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
     }
     
+    func testHasLocationLabel() {
+        XCTAssertNotNil(sut.locationLabel)
+        XCTAssertTrue(sut.locationLabel.isDescendant(of: sut.view))
+    }
+    
     func testHasMapView() {
-        
         XCTAssertNotNil(sut.mapView)
         XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
+    
+    func setupTaskAndAppearanceTransition() {
+        let coordinate = CLLocationCoordinate2D(latitude: 56.90397176, longitude: 60.60643257)
+        let location = Location(name: "Baz", coordinate: coordinate)
+        let date = Date(timeIntervalSince1970: 1583020800)
+        let task = Task(title: "Foo", description: "Bar", date: date, location: location)
+        sut.task = task
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+    }
+    
+    func testSettingTaskSetsTitleLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.titleLabel.text, "Foo")
+    }
+    
+    func testSettingTaskSetsDescriptionLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.descriptionLabel.text, "Bar")
+    }
+    
+    func testSettingTaskSetsLocationLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.locationLabel.text, "Baz")
     }
 }
