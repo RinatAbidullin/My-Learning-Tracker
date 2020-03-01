07dd4909794a50416cdab0fe66641638b3b13186
Rinat Abidullin <rinatabidullin@gmail.com>
2020-03-01 11:12:59 +0500

    21. TDD. Unit Testing - 24. DetailViewController Elements

---
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Rinat Abidullin on 01.03.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+}
---
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewControllerTests.swift
+//  ToDoAppTests
+//
+//  Created by Rinat Abidullin on 01.03.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
+
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: DetailViewController.self)) as? DetailViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testHasTitleLabel() {
+        
+        XCTAssertNotNil(sut.titleLabel)
+        XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDescriptionLabel() {
+        
+        XCTAssertNotNil(sut.descriptionLabel)
+        XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDateLabel() {
+        
+        XCTAssertNotNil(sut.dateLabel)
+        XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasMapView() {
+        
+        XCTAssertNotNil(sut.mapView)
+        XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
+}
