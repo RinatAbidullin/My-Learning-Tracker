1605cc7073b3f1d887604447a5e2ce33c71a6d7f
Rinat Abidullin <rinatabidullin@gmail.com>
2020-03-03 22:02:13 +0500

    21. TDD. Unit Testing - 37. NewTaskViewController Dismiss Test

--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/NewTaskViewControllerTests.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/NewTaskViewControllerTests.swift
@@ -112,6 +112,27 @@ class NewTaskViewControllerTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
     }
+    
+    func testSaveDismissesNewTaskViewController() {
+        // given
+        let mockNewTaskViewController = MockNewTaskViewController()
+        mockNewTaskViewController.titleTextField = UITextField()
+        mockNewTaskViewController.titleTextField.text = "Foo"
+        mockNewTaskViewController.descriptionTextField = UITextField()
+        mockNewTaskViewController.descriptionTextField.text = "Bar"
+        mockNewTaskViewController.locationTextField = UITextField()
+        mockNewTaskViewController.locationTextField.text = "Baz"
+        mockNewTaskViewController.addressTextField = UITextField()
+        mockNewTaskViewController.addressTextField.text = "Екатеринбург"
+        mockNewTaskViewController.dateTextField = UITextField()
+        mockNewTaskViewController.dateTextField.text = "01.03.20"
+        
+        // when
+        mockNewTaskViewController.save()
+        
+        // than
+        XCTAssertTrue(mockNewTaskViewController.isDismissed)
+    }
 }
 
 extension NewTaskViewControllerTests {
@@ -131,6 +152,18 @@ extension NewTaskViewControllerTests {
         
         override var location: CLLocation? {
             return CLLocation(latitude: mockCoordinate.latitude, longitude: mockCoordinate.longitude)
+        }
+    }
+}
+
+extension NewTaskViewControllerTests {
+    
+    class MockNewTaskViewController: NewTaskViewController {
+        
+        var isDismissed = false
+        
+        override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+            isDismissed = true
         }
     }
 }
