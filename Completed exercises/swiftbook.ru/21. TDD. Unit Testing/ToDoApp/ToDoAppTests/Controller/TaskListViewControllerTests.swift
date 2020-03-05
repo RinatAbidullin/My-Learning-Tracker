ca5b964f407cc6ad36e46a5d721b31052bff5e9b
Rinat Abidullin <rinatabidullin@gmail.com>
2020-03-05 22:58:24 +0500

    21. TDD. Unit Testing - 39. NotificationCenter Test

--- Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/TaskListViewControllerTests.swift
+++ Completed exercises/swiftbook.ru/21. TDD. Unit Testing/ToDoApp/ToDoAppTests/Controller/TaskListViewControllerTests.swift
@@ -85,6 +85,21 @@ class TaskListViewControllerTests: XCTestCase {
         
         XCTAssertTrue((sut.tableView as! MockTableView).isReloaded)
     }
+    
+    func testTappingCellSendsNotification() {
+        let task = Task(title: "Foo")
+        sut.dataProvider.taskManager?.add(task: task)
+        
+        expectation(forNotification: NSNotification.Name(rawValue: "DidSelectRow notification"), object: nil) { notification -> Bool in
+            guard let taskFromNotofication = notification.userInfo?["task"] as? Task else { return false }
+            return task == taskFromNotofication
+        }
+        
+        let tableView = sut.tableView
+        tableView?.delegate?.tableView?(tableView!, didSelectRowAt: IndexPath(row: 0, section: 0))
+        
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
 
  extension TaskListViewControllerTests {
